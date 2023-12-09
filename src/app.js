35a1356482703f277027b68ca38285ffb4c7ac6f
stlmargy <stellahmargy0211@gmail.com>
2023-12-09 14:56:02 +0300

    adds JS code for weather api intergration

--- src/app.js
+++ src/app.js
@@ -19,12 +19,31 @@ function displayRealTime(date){
 }
 
 displayRealTime(currentDate);
+function getWeatherData(response){
+    console.log(response.data);
+    let weatherDescriptionElement=document.querySelector("#weather-description");
+    let weatherHumidityElement=document.querySelector(".weather-humidity");
+    let weatherWindElement=document.querySelector(".weather-speed");
+    let weatherTemp=document.querySelector(".weather-temp");
+    weatherTemp.innerHTML=`${Math.floor(response.data.temperature.current)}`;
+    weatherDescriptionElement.innerHTML=`${response.data.condition.description}`;
+    weatherHumidityElement.innerHTML=`${response.data.temperature.humidity}%`;
+    weatherWindElement.innerHTML=`${response.data.wind.speed}km\h`;
+
+
+
+}
 function getCityInputValue(event){
     //stop the page from reloading
     event.preventDefault()
-    let cityInputElement=document.querySelector(".city-input");
+    let cityInputElement=document.querySelector(".city-input").value;
     let h1=document.querySelector("h1");
-    h1.innerHTML=cityInputElement.value
+    h1.innerHTML=cityInputElement
+    //api intergration
+    let apiKey="bfcoa2306cb6b50a21d693ee1219t034";
+    let apiUrl=`https://api.shecodes.io/weather/v1/current?query=${cityInputElement}&key=${apiKey}`;
+    console.log(apiUrl);
+    axios.get(apiUrl).then(getWeatherData)
 }
 let searchForm=document.querySelector("#search-form");
-searchForm.addEventListener("submit",getCityInputValue)
+searchForm.addEventListener("submit",getCityInputValue);
